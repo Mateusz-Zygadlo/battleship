7586efc252150e5a9a6d7de8d265f2cf2c53606d
Mateusz <mateuszzygadlo00@gmail.com>
2021-09-25 21:35:36 +0200

    Add function which make is possible change the position of a object

--- src/components/createShip.js
+++ src/components/createShip.js
@@ -1,31 +1,31 @@
-const createShip = () => {
-    const shipNameAndSize = [
-        {name: 'Destroyer', size: 2, id: 2},
-        {name: 'Submarine', size: 3, id: 3},
-        {name: 'Cruiser', size: 3, id: 3},
-        {name: 'Battleship', size: 4, id: 4},
-        {name: 'Carrier', size: 5, id: 5},
-    ]
+const createShip = (shipNameAndSize) => {
     const shipsClass = document.querySelector('.ships');
 
+    shipsClass.textContent = '';
+
     for(let i = 0; i < shipNameAndSize.length; i++){
-        const ship = document.createElement('div');
-        ship.classList.add('ship');
-        ship.dataset.id = shipNameAndSize[i].id;
-        ship.style.gridTemplateColumns = `repeat(${shipNameAndSize[i].size}, 1fr)`;
-        ship.style.width = `calc(${shipNameAndSize[i].id} * 50px)`;
-        ship.style.height = '50px';
-        ship.style.backgroundColor = 'black'; 
-        ship.draggable = true;
+        if(shipNameAndSize[i].land == true){
+            console.log('is land');
+        }else{
+            const ship = document.createElement('div');
+            ship.classList.add('ship');
+            ship.dataset.id = shipNameAndSize[i].id;
+            ship.classList.add(shipNameAndSize[i].name);
+            ship.style.gridTemplateColumns = `repeat(${shipNameAndSize[i].size}, 1fr)`;
+            ship.style.width = `calc(${shipNameAndSize[i].id} * 50px)`;
+            ship.style.height = '50px';
+            ship.style.backgroundColor = 'black'; 
+            ship.draggable = true;
 
-        for(let j = 0; j < shipNameAndSize[i].size; j++){
-            const square = document.createElement('div');
-            square.classList.add('square');
+            for(let j = 0; j < shipNameAndSize[i].size; j++){
+                const square = document.createElement('div');
+                square.classList.add('square');
 
-            ship.appendChild(square);
-        }   
+                ship.appendChild(square);
+            }   
 
-        shipsClass.appendChild(ship);
+            shipsClass.appendChild(ship);
+        }
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import gameBoardFields from "./components/gameBoardFields";
 import createShip from "./components/createShip";
 
+
+const shipNameAndSize = [
+    {name: 'Destroyer', size: 2, id: 2, land: false},
+    {name: 'Submarine', size: 3, id: 3, land: false},
+    {name: 'Cruiser', size: 3, id: 3, land: false},
+    {name: 'Battleship', size: 4, id: 4, land: false},
+    {name: 'Carrier', size: 5, id: 5, land: false},
+]
+
 const ship = (bodyLength, whereHits, sunk) => {
     
     return{
@@ -28,10 +37,22 @@ const isSunk = (bodyLength) => {
 }
 
 gameBoardFields();
-createShip();
+createShip(shipNameAndSize);
 
 let dragged;
 
+const gameFieldsArr = [];
+let count = 0;
+
+for(let i = 0; i < 10; i++){
+    const newArr = [];
+
+    for(let j = 0; j < 10; j++){
+        newArr.push(count);
+        count++;
+    }
+    gameFieldsArr.push(newArr);
+}
 
 document.addEventListener("dragstart", (e) => {
     dragged = e.target;
@@ -65,12 +86,71 @@ document.addEventListener("dragleave", (e) => {
 
 document.addEventListener("drop", (e) => {
     e.preventDefault();
-    if (e.target.className == "gameField") {
+
+    if(e.target.className == 'gameField'){
         e.target.style.background = "";
-        dragged.parentNode.removeChild(dragged);
         e.target.appendChild(dragged);
-        console.log(e.target.nextSibling);
-        console.log(e.target.childNodes[0].firstChild);
-        console.log(e.target.childNodes[0].lastChild);
+
+        let shipIsGoodSize = false;
+ 
+        let resultPosition = e.target.dataset.row + e.target.dataset.column;
+
+        let stringToInt = Number(resultPosition);
+
+        let row = resultPosition.split('')[0];
+        let column = resultPosition.split('')[1];
+    
+        for(let i = 0; i < resultPosition.split('').length; i++){
+            if(resultPosition.split('')[0] == 0){
+                resultPosition = resultPosition.split('')[1];
+            }
+        }   
+
+        const nameShip = e.target.firstChild.className.split(' ')[1];
+        let lengthShip;
+
+        let indexObj;
+        
+        for(let j = 0; j < shipNameAndSize.length; j++){
+            if(shipNameAndSize[j].name == nameShip){
+                lengthShip = shipNameAndSize[j].size;
+                indexObj = j
+            }
+        }
+        const firstPosition = stringToInt;
+        const lastShipPosition = stringToInt + lengthShip - 1;
+        const shipArrPosition = [];
+
+        for(let x = firstPosition; x <= lastShipPosition; x++){
+            shipArrPosition.push(x);
+        }
+
+        const isTrue = (gameFieldsArr, firstPosition, lastShipPosition, shipIsGoodSize) => {
+            for(let n = 0; n < gameFieldsArr.length; n++){
+                if(gameFieldsArr[n].includes(firstPosition)){
+                    if(gameFieldsArr[n].includes(lastShipPosition)){
+                        shipIsGoodSize = true;
+                        shipNameAndSize[indexObj].land = true;
+    
+                        return shipIsGoodSize;
+                    }; 
+                
+                    return shipIsGoodSize = false;
+                }
+            }
+        }
+
+        if(!isTrue(gameFieldsArr, firstPosition, lastShipPosition, shipIsGoodSize)){
+            dragged.parentNode.removeChild(dragged);
+            e.target.style.background = "";
+            createShip(shipNameAndSize);
+            shipNameAndSize[indexObj].land = false;
+        }else{
+            e.target.style.background = "";
+            dragged.parentNode.removeChild(dragged);
+            e.target.appendChild(dragged);
+        }
+
+        createShip(shipNameAndSize);  
     }
 }, false);
